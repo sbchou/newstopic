1283cd0f8abcde4fbb564b2963b45eb8bccae265
Cat <Cat@CatBook.local>
2015-12-16 23:56:07 -0500

    fix upload script and add header images

--- scripts/upload_production.sh
+++ scripts/upload_production.sh
@@ -1,6 +1,6 @@
 
 TODAY='01-12-2015' &&
-CMD=`meteor mongo -U sorting-hat.meteor.com | tail -1 | sed 's_mongodb://\([a-z0-9\-]*\):\([a-f0-9\-]*\)@\(.*\)/\(.*\)_mongoimport -u \1 -p \2 -h \3 -d \4 -c articles --type json --jsonArray_'` &&
+CMD=`meteor mongo -U newstopic.meteor.com | tail -1 | sed 's_mongodb://\([a-z0-9\-]*\):\([a-f0-9\-]*\)@\(.*\)/\(.*\)_mongoimport -u \1 -p \2 -h \3 -d \4 -c articles --type json --jsonArray_'` &&
 
 $CMD data/$TODAY/$TODAY-CLEAN.json &&
 echo 'imported files to production'
